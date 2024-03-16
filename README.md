3b30696d7c5bcdb5bb1ff40e2fe0acc60e8c9621
Max <max015012@gmail.com>
2024-03-16 18:56:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo
+
+Todo app is a task tracker designed to help users manage their daily tasks effectively
---
+++ src/test/java/com/max015012/todo/TodoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.max015012.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
